fc34525180a52962b21934266bf4feda2b25b675
ryan-limabean <ryan@limabean.co.za>
2018-07-16 16:47:38 +0200

    my first

---
+++ src/Home.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+  <app-servers v-for="server in 5"></app-servers>
+  </div>
+</template>
+
+
+<script>
+  import ServerStatus from './ServerStatus.vue';
+
+    export default {
+    components: {
+        'app-server-status': ServerStatus
+    }
+    
+}
+</script>
