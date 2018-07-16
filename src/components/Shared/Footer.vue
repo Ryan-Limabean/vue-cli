33b1061a17f3f39c04ac8a4a660f15f807705750
unknown <ryan@limabean.co.za>
2018-07-16 19:35:51 +0200

    Ryan

---
+++ src/components/Shared/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+      <h3>footer</h3>
+  </div> 
+</template>
+
+
+<script>
+
+</script>
+
+<style scoped>
+ 
+</style>
+
---
+++ src/components/Shared/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+      <h3>Header</h3>
+  </div> 
+</template>
+
+
+<script>
+
+</script>
+
+<style scoped>
+</style>
+
